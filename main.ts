d213cb3c16d103f4e615f56d65c3f027ba665a7a
Mathieu Lala <56253288+Mathieu-Lala@users.noreply.github.com>
2025-12-15 11:32:25 +0100

    Refactor code structure for improved readability and maintainability

--- main.ts
+++ main.ts
@@ -5,18 +5,15 @@ type E = "Division by zero error";
 const safeDivide = (a: number, b: number): Result<number, E> =>
   b === 0 ? err("Division by zero error") : ok(a / b);
 
-const asyncDivide = async (
-  a: number,
-  b: number
-): Promise<Result<number, E>> => {
+const asyncDivide = async (a: number, b: number): Promise<number> => {
   await new Promise((resolve) => setTimeout(resolve, 1));
-  return safeDivide(a, b);
+  return a / b;
 };
 
 const asyncSafeDivide = async (
   a: number,
   b: number
-): Promise<Result<number, "Division by zero error">> => {
+): Promise<Result<number, E>> => {
   await new Promise((resolve) => setTimeout(resolve, 1));
   return safeDivide(a, b);
 };
@@ -31,9 +28,7 @@ safeDivide(6, 0);
 asyncDivide(6, 0);
 
 // FIXME: This line should trigger both @typescript-eslint/no-floating-promises and neverthrow/must-use-result
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 asyncSafeDivide(6, 0);
 
 // FIXME: This line should trigger both @typescript-eslint/no-floating-promises and neverthrow/must-use-result
-// eslint-disable-next-line neverthrow/must-use-result
 asyncSafeDivide2(6, 2);
