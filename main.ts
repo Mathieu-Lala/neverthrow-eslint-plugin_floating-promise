3b517392bedc1cfa91dd299bb682c75d0535750a
Mathieu Lala <56253288+Mathieu-Lala@users.noreply.github.com>
2026-02-16 12:52:58 +0100

    feat: add asyncSafeDivide to main.ts for asynchronous safe division

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { safeTry } from "neverthrow";
 import "./promise.js";
-import { E, safeDivide } from "./lib.js";
+import { asyncSafeDivide, E, safeDivide } from "./lib.js";
 
 const res = safeTry<number, E>(function* () {
   const a = yield* safeDivide(10, 0);
@@ -13,4 +13,17 @@ if (res.isErr()) {
   console.error(res.error);
 } else {
   console.log(res.value);
+}
+
+const res2 = await safeTry<number, E>(async function* () {
+  const a = yield* await asyncSafeDivide(10, 0);
+  const b = yield* await asyncSafeDivide(20, 5);
+
+  return asyncSafeDivide(a, b);
+});
+
+if (res2.isErr()) {
+  console.error(res2.error);
+} else {
+  console.log(res2.value);
 }
