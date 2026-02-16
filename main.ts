66627c5825b3122828ce554a5f1a816ff9c4e836
Mathieu Lala <56253288+Mathieu-Lala@users.noreply.github.com>
2026-02-16 12:51:13 +0100

    feat: implement safeTry with generator function for safe division

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
+import { safeTry } from "neverthrow";
 import "./promise.js";
+import { E, safeDivide } from "./lib.js";
+
+const res = safeTry<number, E>(function* () {
+  const a = yield* safeDivide(10, 0);
+  const b = yield* safeDivide(20, 5);
+
+  return safeDivide(a, b);
+});
+
+if (res.isErr()) {
+  console.error(res.error);
+} else {
+  console.log(res.value);
+}
